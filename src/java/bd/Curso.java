9a62b47c69bfe1c20c2f93669f9eb4964c0d5bfe
admin-harold.rojas <admin-harold.rojas@FIA-LAB7PROF.lim.upeu.edu.pe>
2014-07-16 16:19:59 -0500

    clase curso

---
+++ src/java/bd/Curso.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bd;
+
+/**
+ *
+ * @author admin-harold.rojas
+ */
+public class Curso {
+    private String nom_curso;
+
+    public Curso() {
+    }
+
+    public Curso(String nom_curso) {
+        this.nom_curso = nom_curso;
+    }
+
+    public String getNom_curso() {
+        return nom_curso;
+    }
+
+    public void setNom_curso(String nom_curso) {
+        this.nom_curso = nom_curso;
+    }
+    
+}
